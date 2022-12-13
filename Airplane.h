dfec47056581b6fe7950a9a6ed099cc845ddd89c
krsnprvl <99241605+krsnprvl@users.noreply.github.com>
2022-12-14 00:21:32 +0300

    Add files via upload

---
+++ Airplane.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Carrier.h"
+
+class Airplane : public Carrier
+{
+	string type;
+	string name;
+	int volume;
+	int size;
+	string city;
+public:
+	Airplane();
+	Airplane(ifstream &fin);
+	~Airplane();
+	void edit();
+	void save(ofstream& fout);
+	void print(ostream& out);
+};
